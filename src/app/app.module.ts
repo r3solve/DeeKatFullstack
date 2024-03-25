47a88e3c6fafd6e4be5d4b8ac912abd3924e431d
r3solve <yelpoeayahaya@gmail.com>
2024-03-25 05:04:26 +0000

    Just made some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartModule } from './pages/cart/cart.module';
 import { SharedModule } from './shared/shared.module';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBdtwc5l7_xmJ5xwHzRZSyqct56n9bv4S8",
+
+  authDomain: "deekart-f5c19.firebaseapp.com",
+
+  projectId: "deekart-f5c19",
+
+  storageBucket: "deekart-f5c19.appspot.com",
+
+  messagingSenderId: "569683536019",
+
+  appId: "1:569683536019:web:7804a8e76c2997cfdfc566"
+
+};
+
 
 
 @NgModule({
@@ -13,10 +30,13 @@ import { SharedModule } from './shared/shared.module';
     AppComponent,
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     SharedModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
